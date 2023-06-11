50bca0cef47372e7e85a38a2989e13fcc47a5580
ev0clu <ev0clu@gmail.com>
2023-06-11 15:38:09 +0200

    Chart style is updated

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -19,34 +19,115 @@ const Main = ({ theme }: MainProps) => {
       chart.update({
         chart: {
           backgroundColor: theme === 'light' ? '#FAFAFA' : '#161A1E'
-        }
+        },
+        title: {
+          style: { color: theme === 'light' ? '#161A1E' : '#FAFAFA' }
+        },
+        xAxis: [
+          {
+            lineColor: theme === 'light' ? '#161A1E' : '#FAFAFA',
+            tickColor: theme === 'light' ? '#161A1E' : '#FAFAFA',
+            labels: {
+              style: {
+                color: theme === 'light' ? '#161A1E' : '#FAFAFA'
+              }
+            }
+          }
+        ],
+        yAxis: [
+          {
+            lineColor: theme === 'light' ? '#161A1E' : '#FAFAFA',
+            tickColor: theme === 'light' ? '#161A1E' : '#FAFAFA',
+            labels: {
+              style: {
+                color: theme === 'light' ? '#161A1E' : '#FAFAFA'
+              }
+            },
+            gridLineColor: theme === 'light' ? '#e6e6e6' : '#78716c'
+          }
+        ]
       });
     }
   }, [theme]);
 
   const [options, setOptions] = useState({
-    rangeSelector: {
-      selected: 1
-    },
     chart: {
       type: 'candlestick',
-      width: '1000',
       height: '500',
       backgroundColor: theme === 'light' ? '#FAFAFA' : '#161A1E'
     },
-    plotOptions: {
-      candlestick: {
-        color: 'red',
-        upColor: 'green',
-        lineColor: 'green',
-        borderColor: 'green',
-        fillOpacity: 1,
+    title: {
+      text: 'BTC/USDT',
+      align: 'left',
+      style: { color: theme === 'light' ? '#161A1E' : '#FAFAFA' }
+    },
+    rangeSelector: {
+      selected: 1,
+      inputStyle: {
+        color: '#0ea5e9'
+      },
+      labelStyle: {
+        color: '#a8a29e'
+      },
+      buttonTheme: {
+        // styles for the buttons
+        fill: 'none',
+        stroke: 'none',
+        'stroke-width': 0,
+        r: 8,
+        style: {
+          color: '#0ea5e9',
+          fontWeight: 'bold'
+        },
         states: {
           hover: {
-            color: 'red',
-            lineColor: 'red'
+            fill: 'rgb(168, 162, 158,0.2)'
+          },
+          select: {
+            fill: '#039',
+            style: {
+              color: 'white'
+            }
           }
+          // disabled: { ... }
         }
+      }
+    },
+    xAxis: [
+      {
+        lineColor: theme === 'light' ? '#161A1E' : '#FAFAFA',
+        tickColor: theme === 'light' ? '#161A1E' : '#FAFAFA',
+        labels: {
+          style: { color: theme === 'light' ? '#161A1E' : '#FAFAFA' }
+        }
+      }
+    ],
+    yAxis: [
+      {
+        lineColor: theme === 'light' ? '#161A1E' : '#FAFAFA',
+        tickColor: theme === 'light' ? '#161A1E' : '#FAFAFA',
+        labels: {
+          style: { color: theme === 'light' ? '#161A1E' : '#FAFAFA' }
+        },
+        gridLineColor: theme === 'light' ? '#e6e6e6' : '#78716c'
+      }
+    ],
+    plotOptions: {
+      candlestick: {
+        color: '#F6465D',
+        upColor: '#0ECB81',
+        lineColor: '#F6465D',
+        upLineColor: '#0ECB81',
+        borderColor: 'black',
+        fillOpacity: 1
+      }
+    },
+    navigator: {
+      series: {
+        color: 'green',
+        lineColor: '#0ea5e9',
+        lineWidth: 1.2,
+        fillColor: 'rgb(14, 165, 233, 0.1)'
       }
     },
     series: [
@@ -565,7 +646,7 @@ const Main = ({ theme }: MainProps) => {
   });
 
   return (
-    <main className="flex flex-1 flex-col items-center justify-center">
+    <main className="my-auto px-12">
       <HighchartsReact
         highcharts={Highcharts}
         constructorType={'stockChart'}
